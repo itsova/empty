17085b771d4c64f0d501b328ffd40501906d2397
Vas <anastasiyavolkova165@gmail.com>
2018-10-28 20:52:45 +0300

    second class

--- src/ru/itsova/tests/Main.java
+++ src/ru/itsova/tests/Main.java
@@ -1,3 +1,5 @@
+package ru.itsova.tests;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/ru/itsova/tests/Test1.java
@@ -0,0 +1,7 @@
+package ru.itsova.tests;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, itsova!");
+    }
+}
